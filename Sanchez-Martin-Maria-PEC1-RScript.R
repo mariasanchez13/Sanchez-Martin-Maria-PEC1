8099d2fd43885c484af2cbf399c6f1d920ec5fae
mariasanchez13 <maria.sanma00@gmail.com>
2024-11-06 10:54:56 +0100

    Add files via upload

--- Sanchez-Martin-Maria-PEC1-RScript.R
+++ Sanchez-Martin-Maria-PEC1-RScript.R
@@ -2,7 +2,7 @@ data <- read.csv("metaboData/Datasets/2024-Cachexia/human_cachexia.csv",
                  row.names = 1) # La primera columna contiene los identificadores
 
 
-metadata <- DataFrame(data$Muscle.loss) # Metadatos, la primera columna contiene inrformación sobre la pérdida muscular
+metadata <- as.data.frame(data$Muscle.loss) # Metadatos, la primera columna contiene inrformación sobre la pérdida muscular
 expressiondata <- t(as.matrix(data[,-1])) # Convertimos los datos de expresión en matriz
 rownames(metadata) <- rownames(data)
 colnames(metadata) <- 'Muscle.loss'
@@ -34,13 +34,29 @@ metadata(se)
 data_analysis <- assay(se, "counts")
 head(data_analysis)
 
+# Kolgomorov-Smirnov
+normal_result <- list()
+
+for (col in names(data)[-1]) {
+  # Grupos
+  control <- data[data$Muscle.loss == 'control', col]
+  dis <- data[data$Muscle.loss == 'cachexic', col]
+  # Prueba Kolgomorov-Smirnov
+  prueba_control <- ks.test(control, "pnorm", mean = mean(control), sd = sd(control))
+  prueba_dis <- ks.test(dis, "pnorm", mean = mean(dis), sd = sd(dis))
+  
+  normal_result[[col]] <- list(control_p = prueba_control$p.value, dis_p = prueba_dis$p.value)
+}
+
+normalidad <- names(normal_result)[sapply(normal_result, function(res) res$control_p > 0.05 & res$dis_p > 0.05)]
+normalidad
 
 # ANOVA
 table(data$Muscle.loss)
 ANOVA_result <- list()
 
 # Realizar ANOVA para cada característica
-for (col in names(data)[-1]) {
+for (col in normalidad) {
   anova_mod <- aov(data[[col]] ~ Muscle.loss, data = data)
   ANOVA_result[[col]] <- summary(anova_mod)
 }
@@ -65,4 +81,25 @@ new_data <- data[, c("Muscle.loss", features_sig)]
 
 
 # Tamaño
-significativos
+names(significativos)
+
+
+# Visualización del ANOVA
+library(ggplot2)
+library(patchwork)
+
+grafics <- list() # lista para almacenar los graficos
+for (col in names(significativos)) {
+  # Crear un boxplot para cada característica significativa
+  plots <- ggplot(data, aes(x = Muscle.loss, y = .data[[col]], fill = Muscle.loss)) +
+    geom_boxplot(alpha = 0.7) +
+    labs(title = paste("Boxplot de", col),
+         x = "Grupo",
+         y = col) +
+    scale_fill_manual(values = c("pink", "lightgreen"))
+  
+  # Mostrar el gráfico
+  grafics[[col]] <- plots
+}
+
+wrap_plots(grafics, nrow = 4)
